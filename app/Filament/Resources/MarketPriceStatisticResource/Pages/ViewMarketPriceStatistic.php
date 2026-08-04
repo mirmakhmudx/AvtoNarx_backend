047a17e5e3681b350f570165910d5bed768f2067
Mirmahmud03 <akeforex55@gmail.com>
2026-08-04 15:45:58 +0500

    updated admin panel

---
+++ app/Filament/Resources/MarketPriceStatisticResource/Pages/ViewMarketPriceStatistic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\MarketPriceStatisticResource\Pages;
+
+use App\Filament\Resources\MarketPriceStatisticResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewMarketPriceStatistic extends ViewRecord
+{
+    protected static string $resource = MarketPriceStatisticResource::class;
+}
