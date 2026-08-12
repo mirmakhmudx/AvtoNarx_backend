b0d17246fdb90ec54b645244dda38b78e872289d
Mirmahmud03 <akeforex55@gmail.com>
2026-08-12 16:25:50 +0500

    edit ViewMarketPriceStatistic.php

--- app/Filament/Resources/MarketPriceStatisticResource/Pages/ViewMarketPriceStatistic.php
+++ app/Filament/Resources/MarketPriceStatisticResource/Pages/ViewMarketPriceStatistic.php
@@ -8,4 +8,12 @@ use Filament\Resources\Pages\ViewRecord;
 class ViewMarketPriceStatistic extends ViewRecord
 {
     protected static string $resource = MarketPriceStatisticResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['brand']['name'] = $this->record->brand?->name;
+        $data['carModel']['name'] = $this->record->carModel?->name;
+
+        return $data;
+    }
 }
